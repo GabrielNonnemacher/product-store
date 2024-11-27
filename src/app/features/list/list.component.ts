87b97fe4db33920c0b19d38423e980ee011ff0dc
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-11-27 14:35:12 -0300

    Task #8 -  Adicionando service e db.json

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product } from '../../shared/interfaces/product.interface';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  productsService = inject(ProductsService);
+  products: Product[] = [] as Product[];
+
+  public ngOnInit(): void {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
