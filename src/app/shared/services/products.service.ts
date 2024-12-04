86c6be2cd356e7afc6c79923c3d2ee356ff32035
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-12-03 21:20:56 -0300

    Task #12 - adicionando rota de criar produto

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { ProductPayload } from '../interfaces/payload-product.interface';
 import { Product } from '../interfaces/product.interface';
 
 @Injectable({
@@ -10,5 +11,9 @@ export class ProductsService {
 
   public getAll() {
     return this.httpClient.get<Product[]>('/api/products');
+  }
+
+  public post(payload: ProductPayload) {
+    return this.httpClient.post<Product>('/api/products', payload);
   }
 }
